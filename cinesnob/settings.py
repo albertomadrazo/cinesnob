d9006262530102d041d34a8fdd276ed9657f1345
alberto <otreblatercero@gmail.com>
2015-12-28 17:40:00 -0600

    Begin once more with this endless pain which is deployment

--- cinesnob/settings.py
+++ cinesnob/settings.py
@@ -45,9 +45,12 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'califas',
+    'storages',
 )
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_S3_SECURE_URLS = False
+AWS_QUERYSTRING_AUTH = False
 
-INSTALLED_APPS += ('storages',)
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
 MIDDLEWARE_CLASSES = (
@@ -178,7 +181,6 @@ STATICFILES_DIRS = (
     # os.path.join(BASE_DIR, 'static'),
  )
 
-AWS_QUERYSTRING_AUTH = False
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
@@ -186,6 +188,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 MEDIA_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 STATICFILES_STORAGE = 'storages.backends.s3boto.s3BotoStorage'
 #if DEBUG == False:
+
 #    MEDIA_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 #    STATICFILES_STORAGE = 'storages.backends.s3boto.s3BotoStorage'
  #Access Key ID: AKIAJ4B6GKD6XZD4PFBA
