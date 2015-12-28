c8b0a066ad539d4708a9ecfffee8e5f9b045a4ee
alberto <otreblatercero@gmail.com>
2015-12-28 17:53:48 -0600

    Begin once more with this endless pain which is deployment

--- cinesnob/settings.py
+++ cinesnob/settings.py
@@ -187,7 +187,7 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 
 MEDIA_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-STATICFILES_STORAGE = 'storages.backends.s3boto.s3BotoStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 #if DEBUG == False:
 
 #    MEDIA_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
